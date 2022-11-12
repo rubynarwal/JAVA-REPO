3f99ae30c64d1388570c85f28c82d8f1bb6aa864
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-11-12 23:47:50 +0530

    added

---
+++ src/switchcase/switchc.java
@@ -0,0 +1,34 @@
+package switchcase;
+import java.util.*;
+public class switchc {
+    public static void main(String[] args) {
+        Scanner ac=new Scanner(System.in);
+        int num=ac.nextInt();
+        switch(num){
+
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println( "Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println( "Friday");
+                break;
+            case 6:
+                System.out.println( "Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+
+        }
+    }
+}
