fba57830c53a28e39ab43f9d12807edd3d5e5892
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-11-12 09:43:10 +0530

    added

---
+++ src/pattern/star3.java
@@ -0,0 +1,16 @@
+package pattern;
+
+public class star3 {
+    public static void main(String[] args){
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                if(i>=2&&j>=2 && i<=3&&j<=3){
+                    System.out.print("  ");
+                }else{
+                    System.out.print("*"+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/pattern/stars2.java
@@ -0,0 +1,14 @@
+package pattern;
+
+public class stars2 {
+    public static void main(String[] args){
+        for(int i=1;i<=4;i++){
+            for(int j=4;j>=i;j--) {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+
+
+        }
+    }
+}
