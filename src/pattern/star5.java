923a20dad44a66510737f945310f718f1cd1d7e7
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-11-13 10:28:02 +0530

    added

---
+++ src/pattern/star5.java
@@ -0,0 +1,11 @@
+package pattern;
+
+public class star5 {
+    public static void main(String[] args) {
+
+    for(int i=1;i<=5;i++){
+
+      System.out.println("Ruby Narwal"+" "+"*****");
+        }
+    }
+}
