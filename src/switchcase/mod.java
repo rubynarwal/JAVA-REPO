aff59aab2487025be519af19cfe8b00ae035f098
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-11-14 21:29:05 +0530

    added

---
+++ src/switchcase/mod.java
@@ -0,0 +1,45 @@
+package switchcase;
+import java.util.*;
+public class mod {
+
+        public static void main(String[] args){
+            Scanner pc =new Scanner(System.in);
+            int num=pc.nextInt();
+            switch(num % 10){
+                case 0:
+                    System.out.println( "zero");
+                    break;
+                case 1:
+                    System.out.println( "one");
+                    break;
+
+                case 2:
+                    System.out.println( "two");
+                    break;
+                case 3:
+                    System.out.println( "three");
+                    break;
+                case 4:
+                    System.out.println( "four");
+                    break;
+                case 5:
+                    System.out.println( "five");
+                    break;
+                case 6:
+                    System.out.println( "six");
+                    break;
+                case 7:
+                    System.out.println( "seven");
+                    break;
+                case 8:
+                    System.out.println( "eight");
+                    break;
+                case 9:
+                    System.out.println( "nine");
+                    break;
+
+
+            }
+        }
+    }
+
