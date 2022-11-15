96458fd45dc0df108b45e0a966853032894e4f93
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-11-15 11:29:10 +0530

    git add .

---
+++ src/switchcase/monthsName.java
@@ -0,0 +1,61 @@
+package switchcase;
+import java.util.Scanner;
+
+public class monthsName {
+    public static void main(String[] args) {
+        Scanner pc = new Scanner(System.in);
+        int x= pc.nextInt();
+      switch(x){
+          case 1:
+          System.out.println("January");
+          break;
+
+          case 2:
+              System.out.println("February");
+              break;
+
+          case 3:
+              System.out.println("March");
+              break;
+
+          case 4:
+              System.out.println(" April");
+              break;
+
+          case 5:
+              System.out.println(" May");
+              break;
+
+          case 6:
+              System.out.println("June");
+              break;
+
+          case 7 :
+              System.out.println("July");
+              break;
+
+          case 8:
+              System.out.println("August");
+              break;
+
+          case 9:
+              System.out.println(" September");
+              break;
+
+          case 10:
+              System.out.println("October");
+              break;
+
+          case 11:
+              System.out.println(" November");
+              break;
+
+          case 12:
+              System.out.println("December");
+              break;
+
+
+
+      }
+    }
+}
