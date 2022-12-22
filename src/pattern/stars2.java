49beff8d75fe42aba2dc3ffee279b5f369b0d102
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-22 23:25:10 +0530

    added

--- src/pattern/stars2.java
+++ src/pattern/stars2.java
@@ -2,8 +2,8 @@ package pattern;
 
 public class stars2 {
     public static void main(String[] args){
-        for(int i=1;i<=5;i++){
-            for(int j=5;j>=i;j--) {
+        for(int i=1;i<=7;i++){
+            for(int j=7;j>=i;j--) {
                 System.out.print("*"+" ");
             }
             System.out.println();
