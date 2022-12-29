a43cb4ff05b49a742c8fe5f5acf3c81d0ccb4991
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-29 19:24:19 +0530

    added

---
+++ src/calculator/Examples.java
@@ -0,0 +1,37 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Examples {
+
+  public static void main(String[] args) {
+//        int a = 2;
+//        int b = 3;
+//        int c = 5;
+//        int sum=a+b+c;
+//        System.out.println(sum);
+
+//      Question 2---->
+//      float sub1=60;
+//      float sub2=60;
+//      float sub3=70;
+//      float cgpa=(sub1+sub2+sub3)/30;
+//      System.out.println(cgpa);
+//
+//      question 3--------->
+
+      System.out.println("How are you");
+      Scanner ac=new Scanner(System.in);
+
+    String name= ac.next();
+      System.out.println("Hello" +" " +name+"I am great");
+
+
+
+
+    }
+}
+
+
+
+
