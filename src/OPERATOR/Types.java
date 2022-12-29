2081ce196ff04ee2101202e3d739da1d3cae15a9
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-29 20:08:43 +0530

    added

--- src/OPERATOR/Types.java
+++ src/OPERATOR/Types.java
@@ -3,7 +3,8 @@ package OPERATOR;
 public class Types {
     public static void main(String[] args) {
         int a=5;
-        int b=6+a;
+        int b=6;
+         b +=6;
         System.out.println(b);
     }
 
---
+++ src/OPERATOR/ruby.java
@@ -0,0 +1,5 @@
+package OPERATOR;
+
+public class ruby {
+
+}
