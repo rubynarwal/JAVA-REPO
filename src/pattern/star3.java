62863aef585306bc13baae96db2a28f94dc2b8db
RUBY <103845902+rubynarwal@users.noreply.github.com>
2023-02-04 21:42:35 +0530

    ruby narwal

--- src/pattern/star3.java
+++ src/pattern/star3.java
@@ -2,8 +2,8 @@ package pattern;
 
 public class star3 {
     public static void main(String[] args){
-        for(int i=1;i<=4;i++){
-            for(int j=1;j<=4;j++){
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=3;j++){
                 if(i>=2&&j>=2 && i<=3&&j<=3){
                     System.out.print("  ");
                 }else{
